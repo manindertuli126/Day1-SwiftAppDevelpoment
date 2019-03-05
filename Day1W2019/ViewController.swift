7b515423535b8edaa2ae3e5c12d6c2975a4d2108
MacStudent <macstudent@126-imac13.local>
2019-03-04 21:18:13 -0500

    Added calculator code.

--- Day1W2019/ViewController.swift
+++ Day1W2019/ViewController.swift
@@ -12,10 +12,35 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var helloMessage: UILabel!
     @IBOutlet weak var noLabelMessage: UILabel!
+    @IBOutlet weak var Number1: UITextField!
+    @IBOutlet weak var Number2: UITextField!
+    @IBOutlet weak var outputResult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        helloMessage.text = "Hello World!!"
+        //noLabelMessage.text = "Hello World!!"
     }
 
+    @IBAction func btnSubmitClick(_ sender: Any) {
+        noLabelMessage.text = "Hello World Submitted"
+    }
+    
+    @IBAction func addNumber(_ sender: Any) {
+        let a = Int(Number1.text!)
+        let b = Int(Number2.text!)
+        let c = a! + b!
+//        outputResult.text = String(c)
+        outputResult.text = "Add: \(c)"
+    }
+    
+    @IBAction func subNumber(_ sender: Any) {
+        let a = Int(Number1.text!)
+        let b = Int(Number2.text!)
+        let c = a! - b!
+        outputResult.text = "Sub: \(c)"
+    }
+    @IBAction func multiplyNumber(_ sender: Any) {
+    }
+    @IBAction func divideNumber(_ sender: Any) {
+    }
 }
 
