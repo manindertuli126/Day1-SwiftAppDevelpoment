69a04954495e8548d8d15ad0957ee919077ba3b9
MacStudent <macstudent@126-imac13.local>
2019-03-04 20:20:57 -0500

    Added text to storyboard.

--- Day1W2019/ViewController.swift
+++ Day1W2019/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloMessage: UILabel!
+    @IBOutlet weak var noLabelMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        helloMessage.text = "Hello World!!"
     }
-
 
 }
 
